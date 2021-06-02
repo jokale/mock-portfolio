99b3ebd42429fa5bd5a66b60b85de520240b76aa
jokale <kalemajoanna@gmail.com>
2021-06-02 10:04:16 +0100

    build basis of navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,26 +1,67 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import styled from 'styled-components'
+
+
+const Nav = styled.div ` 
+height: 100px;
+background-color: white;
+position: sticky;
+`
+
+const HomeTitle = styled.a`
+position: fixed;
+left: 40px;
+font-family: 'Source Sans Pro', sans-serif;
+padding-top: 45px;
+font-size: 1.4rem;
+border: 1px solid red;
+width: 100%;
+text-decoration: none;
+color: black;
+`
+
+const RestNavOne = styled.a `
+position: fixed;
+right: 440px;
+text-decoration: none;
+display: inline;
+padding-top: 50px;
+padding-left: 30px;
+font-size: 1rem;
+scroll-margin-top: 5rem;
+text-decoration: none;
+`
+
+const RestNavTwo = styled(RestNavOne) `
+position: fixed;
+right: 300px;
+`
+
+
+const RestNavThree = styled(RestNavOne) `
+position: fixed;
+right: 160px;
+`
+
+
+const RestNavFour = styled(RestNavOne) `
+position: fixed;
+right: 40px;
+`
 
 
 function Navbar() {
     return (
       <div className="Navbar">
-          <NavLink to="/"
-          exact
-          >Home</NavLink>
-          <NavLink to="/about-me"
-          exact
-          >ABOUT ME</NavLink>
-          <NavLink to="/resume"
-          exact
-          >RESUME</NavLink>
-          <NavLink to="/projects"
-          exact
-          >PROJECTS</NavLink>
-          <NavLink to="/contact"
-          exact
-          >CONTACT</NavLink>
-
+         <Nav>
+         <HomeTitle> <NavLink to="/" exact>Liezel  Frank / PROJECT MANAGER </NavLink></HomeTitle>
+           <RestNavOne><NavLink to="/about-me" exact  >ABOUT ME</NavLink> </RestNavOne>
+            <RestNavTwo><NavLink to="/resume" exact >RESUME</NavLink></RestNavTwo>
+               <RestNavThree><NavLink to="/projects" exact  >PROJECTS</NavLink></RestNavThree>
+                 <RestNavFour><NavLink to="/contact" exact>CONTACT</NavLink></RestNavFour>
+       
+</Nav>
       </div>
     );
   }
